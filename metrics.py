a1d6ee7bef031bdb9851ec92b0d34da76246ffb8
Guorui Wen <grwen@stu.pku.edu.cn>
2024-06-11 17:50:22 +0800

    change image file naming convention in metrics.py

--- metrics.py
+++ metrics.py
@@ -4,6 +4,7 @@ import numpy as np
 import imageio
 import matplotlib.pyplot as plt
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--scene', type=str, default='cbox')
@@ -13,6 +14,7 @@ args = parser.parse_args()
 name = f'{args.scene}_{args.method}'
 
 def read_exr(file_path):
+    file_path = os.path.realpath(file_path)
     exr_file = OpenEXR.InputFile(file_path)
     header = exr_file.header()
     dw = header['dataWindow']
@@ -51,12 +53,12 @@ def calculate_psnr(image1, image2):
 
 # Paths to the EXR files
 base_path = f'exr/{name}.'
-label_image_path = base_path + '-1.exr'
+label_image_path = f'exr/{args.scene}.standard.exr'
 label_image = read_exr(label_image_path)
 
 mse_values = []
 psnr_values = []
-image_indices = range(1,26)
+image_indices = [2 ** i for i in range(0, 10)]
 
 for i in image_indices:
     image_path = base_path + f'{i}.exr'
